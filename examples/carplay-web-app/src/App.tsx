e87851ee45d6122d2cc2521116a5518decd8c9b9
frankreb <126629522+frankreb@users.noreply.github.com>
2025-01-15 19:30:59 -0500

    bug fix

--- examples/carplay-web-app/src/App.tsx
+++ examples/carplay-web-app/src/App.tsx
@@ -281,8 +281,8 @@ function App() {
         onPointerOut={sendTouchEvent}
         style={{
           position: 'absolute',
-          width: '82%',
-          height: '82%',
+          width: '78%', //82
+          height: '78%', //82
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
